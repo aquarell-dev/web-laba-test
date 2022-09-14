9ddaac39b3a1ed4e094b7e3a7d71ef07b5ab62be
aquarell <aquarell.dev@gmail.com>
2022-09-14 21:04:27 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+
+import VsuLogo from './assets/vsu-logo.png';
+// import { FaFacebook, IoLogoFacebook, MdFacebook } from 'react-icons/all';
+import { BsFacebook, BsInstagram, BsYoutube, BsTelegram } from 'react-icons/bs';
+
+const socialMedia = [
+  <BsFacebook size={25} />,
+  <BsInstagram size={25} />,
+  <BsYoutube size={25} />,
+  <BsTelegram size={25} />
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="centered">
+        <div className="card">
+          <div className="content">
+            <div className="heading">
+              <img
+                src={VsuLogo}
+                alt="Vsu Logo"
+                className="logo"
+              />
+              <span className="title">Воронежский гос. университет</span>
+            </div>
+            <div className="info">
+              <div className="key_phrase">Поступай</div>
+              <div className="social_media">
+                {socialMedia.map((soc_media, idx) => (
+                  <div key={idx} className='social_media_item'>
+                    {soc_media}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div id="trapezoid">
+            <div className="number">
+              <p className="centered">00501</p>
+            </div>
+          </div>
+          <div className="contacts__wrapper">
+            <div className="contacts">
+              <p>8 800 555 35-35</p>
+              <p>vsu.ru</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
